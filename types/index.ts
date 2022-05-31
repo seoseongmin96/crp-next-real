68f6f9c8b606e8c7e6224eb93b5fdc2405666d90
seoseongmin96 <justin090452@gmail.com>
2022-05-31 11:58:15 +0900

    first commit

---
+++ types/index.ts
@@ -0,0 +1,18 @@
+export interface IUSER{
+    userId : number
+    username : string
+    password : string
+    name : string
+    email : string
+    regDate : string
+    token : string
+    roles: string[]
+}
+
+export interface IArticle{
+   
+}
+
+export interface IBoard{
+   
+}
